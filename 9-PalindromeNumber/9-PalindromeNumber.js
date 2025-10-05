c6e02c532fc053d80e3e286721f016e5e339c36f
AnilRayavarap <142166535+AnilRayavarap@users.noreply.github.com>
2025-10-05 08:31:09 -0500

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.js
@@ -0,0 +1,9 @@
+// Last updated: 10/5/2025, 8:31:08 AM
+let isPalindrome = function(x) {
+    let result = Number(String(x).split('').reverse('').join(''))
+    if(result == x) {
+        return true;
+    } else {
+        return false;
+    }
+}
